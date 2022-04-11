831ddc9a303aa6342807c81df0bdde3f105a3315
Jessie <2302541+Kangaroux@users.noreply.github.com>
2022-04-11 15:46:22 -0400

    Add a task for generating the spec, read version from file

--- tasks.sh
+++ tasks.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-case "$1" in
-"openapi-gen")
+set -e
+cd ${0%/*}
+
+function swagger() {
+    local cwd=$(pwd)
+
     docker run \
         --rm \
         -it \
-        --user "$(id -u):$(id -g)" \
         -e GOPATH=$(go env GOPATH):/go \
-        -v $HOME:$HOME \
-        -w $(pwd) \
-        quay.io/goswagger/swagger
+        -e API_VERSION \
+        -v $cwd:$cwd \
+        -w $cwd \
+        quay.io/goswagger/swagger \
+        "$@"
+}
+
+case "$1" in
+"swagger")
+    shift
+    swagger "$@"
+    ;;
+
+"spec")
+    docs_backup=docs/.docs.go
+    out_file=swagger.yml
+
+    # Make a backup of docs.go and substitute the API_VERSION env
+    mv docs/docs.go $docs_backup
+    API_VERSION=`head -n 1 VERSION` envsubst < $docs_backup > docs/docs.go
+
+    # Touch the file first so it has the correct permissions
+    touch $out_file
+    swagger generate spec -o $out_file
+
+    # Restore the backup
+    mv $docs_backup docs/docs.go
     ;;
 esac
