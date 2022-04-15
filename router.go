7e1451bf7053e41519fd8b28739f028db2221566
Jessie <2302541+Kangaroux@users.noreply.github.com>
2022-04-15 00:23:29 -0400

    Add a /schema endpoint that returns the openapi yaml

--- router.go
+++ router.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -12,7 +14,19 @@ func NewAPIRouter(apis RouterAPIs) *mux.Router {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
 
+	api.Path("/schema").HandlerFunc(OpenAPISchemaView)
 	api.PathPrefix("/users").Handler(apis.User.Router("/api/v1/users"))
 
 	return r
+}
+
+// swagger:route GET /schema getSchema
+//
+// Returns a yaml of the OpenAPI schema
+//
+// Produces:
+//   - application/yaml
+func OpenAPISchemaView(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("content-type", "application/yaml")
+	http.ServeFile(w, req, "swagger.yml")
 }
