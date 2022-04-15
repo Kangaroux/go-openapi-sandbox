a3b57a9251f6ff978282c11234c1c9f27a02d864
Jessie <2302541+Kangaroux@users.noreply.github.com>
2022-04-15 00:10:12 -0400

    Move swagger response structs into docs/

---
+++ docs/responses.go
@@ -0,0 +1,27 @@
+package docs
+
+import api "github.com/kangaroux/go-openapi-test"
+
+// swagger:response baseResponse
+type SwaggerBaseResponse struct {
+	// in: body
+	Body api.BaseResponse
+}
+
+// swagger:response fieldErrorResponse
+type SwaggerFieldErrorResponse struct {
+	// in: body
+	Body api.FieldErrorResponse
+}
+
+// swagger:response userResponse
+type SwaggerUserResponse struct {
+	// in: body
+	Body api.UserResponse
+}
+
+// swagger:response listUserResponse
+type SwaggerListUserResponse struct {
+	// in: body
+	Body api.ListUserResponse
+}
--- responses.go
+++ responses.go
@@ -10,22 +10,10 @@ type BaseResponse struct {
 	OK bool `json:"ok"`
 }
 
-// swagger:response baseResponse
-type SwaggerBaseResponse struct {
-	// in: body
-	Body BaseResponse
-}
-
 type FieldErrorResponse struct {
 	// swagger:allOf
 	BaseResponse
 	FieldErrors interface{} `json:"field_errors"`
-}
-
-// swagger:response fieldErrorResponse
-type SwaggerFieldErrorResponse struct {
-	// in: body
-	Body FieldErrorResponse
 }
 
 type UserResponse struct {
@@ -34,23 +22,11 @@ type UserResponse struct {
 	User *User `json:"user,omitempty"`
 }
 
-// swagger:response userResponse
-type SwaggerUserResponse struct {
-	// in: body
-	Body UserResponse
-}
-
 type ListUserResponse struct {
 	// swagger:allOf
 	BaseResponse
 	Count int     `json:"count"`
 	Users []*User `json:"users"`
-}
-
-// swagger:response listUserResponse
-type SwaggerListUserResponse struct {
-	// in: body
-	Body ListUserResponse
 }
 
 func NewFieldErrorResponse(fieldErrors interface{}, msg string) FieldErrorResponse {
